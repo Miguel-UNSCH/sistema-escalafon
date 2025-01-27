0e7faf7a324749c6f116d152572aca1a9cd89d2d
legion-commander <lcommander101010@gmail.com>
2025-01-27 15:35:25 -0500

    feat: add catppuccin flavours for tailwindcss

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -6,9 +6,9 @@ interface NavbarProps {
 
 export default function Navbar({ onMenuClick }: NavbarProps) {
   return (
-    <nav className="shadow-md p-4">
+    <nav className="shadow-md p-4 text-ctp-text">
       <div className="flex items-center">
-        <button onClick={onMenuClick} className="mr-4">
+        <button onClick={onMenuClick} className="mr-4 hover:bg-slate-100 rounded-full p-1">
           <HiOutlineMenuAlt1 className="h-6 w-6" />
         </button>
         <h1 className="text-xl font-semibold">My Dashboard</h1>
