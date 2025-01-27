bd26c2368fdef7b2cab5f17a97cca2410495403b
legion-commander <lcommander101010@gmail.com>
2025-01-27 16:18:16 -0500

    feat: other changes

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -6,12 +6,12 @@ interface NavbarProps {
 
 export default function Navbar({ onMenuClick }: NavbarProps) {
   return (
-    <nav className="shadow-md p-4 text-ctp-text">
+    <nav className="shadow-md p-4 text-ctp-text ctp-mocha">
       <div className="flex items-center">
         <button onClick={onMenuClick} className="mr-4 hover:bg-slate-100 rounded-full p-1">
           <HiOutlineMenuAlt1 className="h-6 w-6" />
         </button>
-        <h1 className="text-xl font-semibold">My Dashboard</h1>
+        <h1 className="text-xl font-semibold text-ctp-red">My Dashboard</h1>
       </div>
     </nav>
   )
