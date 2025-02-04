981224067275c261edf823b02f30ca62e19b7412
lc <lcommander101010@gmail.com>
2025-02-03 21:50:46 -0500

    :( !!!

--- src/app/dashboard/personal-file/personal-information/children-data/page.tsx
+++ src/app/dashboard/personal-file/personal-information/children-data/page.tsx
@@ -1,108 +1,138 @@
 "use client";
-import { InputTypea } from "@/components/forms/InputTypes";
-import { InputsChildrenData } from "@/types";
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { SubmitHandler, useForm, useFormContext } from "react-hook-form";
 
-interface IFormData {
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+type InputTextProps = {
   label: string;
-  name: keyof InputsChildrenData;
-  size?: string;
-  type: "input" | "select";
-  options: InputTypeForm | SelectTypeForm;
-}
-interface InputTypeForm {
+  name: string;
   placeholder: string;
-}
+  errorMessage: {
+    required: string;
+    minLength: { value: number; message: string };
+    maxLength: { value: number; message: string };
+  };
+};
 
-interface SelectTypeForm {
-  items: string[];
-  defaultValue: string;
-}
+const InputText: React.FC<InputTextProps> = ({ label, name, placeholder, errorMessage }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
 
-const FormTemplatea = () => {
-  const { control, handleSubmit } = useForm<InputsChildrenData>();
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        placeholder={placeholder}
+        {...register(name, {
+          required: errorMessage.required,
+          minLength: errorMessage.minLength,
+          maxLength: errorMessage.maxLength,
+        })}
+        className="bg-transparent border-none outline-none"
+      />
+      {errors[name] && <p>{errors[name]?.message}</p>}
+    </div>
+  );
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputNumber = () => {
+  return <div>InputNumber</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputDate = () => {
+  return <div>InputDate</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputDates = () => {
+  return <div>InputDates</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputCheckbox = () => {
+  return <div>InputCheckbox</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputFullName = () => {
+  return <div>InputFullName</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputLocation = () => {
+  return <div>InputLocation</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputTextArea = () => {
+  return <div>InputTextArea</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputDocument = () => {
+  return <div>InputDocument</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
+export const InputPeriod = () => {
+  return <div>InputPeriod</div>;
+};
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
 
-  const formData: IFormData[] = [
-    {
-      label: "Apellidos y nombres",
-      name: "fullName",
-      type: "input",
-      size: "medium",
-      options: {
-        placeholder: "Ingrese sus apellidos y nombres",
-      },
-    },
-    {
-      label: "Lugar y fecha de nacimiento",
-      name: "birthPlaceAndDate",
-      type: "input",
-      size: "medium",
-      options: {
-        placeholder: "Ingrese lugar y fecha de nacimiento",
-      },
-    },
-    {
-      label: "Edad",
-      name: "age",
-      type: "input",
-      size: "small",
-      options: {
-        placeholder: "Ingrese su edad",
-      },
-    },
-    {
-      label: "Grado de instrucción",
-      name: "educationLevel",
-      type: "input",
-      size: "medium",
-      options: {
-        placeholder: "Ingrese su grado de instrucción",
-      },
-    },
-  ];
+type FormValues = {
+  name: string;
+  email: string;
+};
 
-  const onSubmit: SubmitHandler<InputsChildrenData> = (data) =>
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {formData.map((field, index) => (
-        <InputTypea
-          key={index}
-          label={field.label}
-          name={field.name as keyof InputsChildrenData}
-          type={field.type}
-          control={control}
-          options={field.options}
+      <InputText
+        label="Nombre"
+        name="name"
+        placeholder="Ingrese su nombre"
+        errorMessage={{
+          required: "Este campo es obligatorio",
+          minLength: { value: 3, message: "Debe tener al menos 3 caracteres" },
+          maxLength: { value: 50, message: "Debe tener como máximo 50 caracteres" },
+        }}
+      />
+
+      <div>
+        <label htmlFor="email">Correo Electrónico</label>
+        <input
+          id="email"
+          type="email"
+          {...register("email", {
+            required: "Este campo es obligatorio",
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: "El correo electrónico no es válido",
+            },
+          })}
+          className="bg-transparent border-none outline-none"
         />
-      ))}
-      <div className="flex flex-row justify-between w-full text-[#eff1f5]">
-        <button
-          type="button"
-          className="justify-center items-center bg-[#d20f39] px-4 py-2 rounded-lg"
-        >
-          Volver
-        </button>
-        <button
-          type="submit"
-          className="justify-center items-center bg-[#40a02b] ml-auto px-4 py-2 rounded-lg"
-        >
-          Enviar
-        </button>
+        {errors.email && <p>{errors.email.message}</p>}
       </div>
+
+      <button type="submit">Enviar</button>
     </form>
   );
 };
+/** ---------------------------------------------------------------------------------------------------------------------------------------------- */
 
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center font-poppins">
       <div className="flex flex-col">
-        <h3 className="font-bold font-montserrat text-xl uppercase">
-          Datos de los hijos
-        </h3>
-        <FormTemplatea />
+        <h3 className="font-bold font-montserrat text-xl uppercase">Datos de los hijos</h3>
+        <Form />
       </div>
     </div>
   );
--- src/utils/bonuses-evaluations.ts
+++ src/utils/bonuses-evaluations.ts
@@ -7,27 +7,27 @@
 
 /**
  * -- Bonificación personal (+ trabajador)
- * Apellidos y nombres
- * Tipo de bonificación personal
- * Resolución de bonificación personal
- * Dependencia y/o oficina
- * Cargo
+ * Apellidos y nombres  -- input
+ * Tipo de bonificación personal  -- select
+ * Resolución de bonificación personal  -- input
+ * Dependencia y/o oficina  -- select | input
+ * Cargo -- input
  */
 
 /**
  * -- Bonificación familiar (+ trabajador)
- * Apellidos y nombres
- * Tipo de Bonificación familiar
- * Resolución de bonificación familiar
- * Dependencia y/o oficina
- * Cargo
+ * Apellidos y nombres  -- input
+ * Tipo de Bonificación familiar -- select
+ * Resolución de bonificación familiar -- input
+ * Dependencia y/o oficina -- select | input
+ * Cargo -- input
  */
 
 /**
  * -- Ficha de evaluación (+ trabajador)
- * Apellidos y nombres
- * Documento de sustento
- * Dependencia y/o oficina
- * Cargo
- * Fecha de evaluación
+ * Apellidos y nombres -- input
+ * Documento de sustento -- input
+ * Dependencia y/o oficina -- select | input
+ * Cargo -- input
+ * Fecha de evaluación -- input
  */
