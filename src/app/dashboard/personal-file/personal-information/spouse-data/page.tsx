ddf327b316687d106c56931be5213179a76b95b3
lc <lcommander101010@gmail.com>
2025-02-07 15:58:25 -0500

    :(

--- src/app/dashboard/personal-file/personal-information/spouse-data/page.tsx
+++ src/app/dashboard/personal-file/personal-information/spouse-data/page.tsx
@@ -411,13 +411,14 @@ const Form = () => {
     </form>
   );
 };
+
 /** ---------------------------------------------------------------------------------------------------------------------------------------------- */
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center font-poppins text-[#11111b]">
       <div className="flex flex-col justify-center items-center gap-4 bg-white p-8 rounded-lg">
         {/** si el personal es M (esposa) si es F (esposo) */}
-        <h3 className="font-bold font-montserrat text-2xl text-center uppercase">datos del esposa</h3>
+        <h3 className="font-bold font-montserrat text-2xl text-center uppercase">datos de la esposa</h3>
         <div className="flex-row justify-evenly items-center gap-2 hidden">
           <span className="flex justify-center items-center bg-[#ccd0da] rounded-full w-8 h-8">1</span>
           <div className="flex-grow bg-[#ccd0da] h-1"></div>
