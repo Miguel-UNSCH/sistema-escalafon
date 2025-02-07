e329df8c31c68d683ffa1a123f8332fd4de2868b
lc <lcommander101010@gmail.com>
2025-02-07 16:40:12 -0500

    :(

--- src/app/dashboard/work-situation/displacement/page.tsx
+++ src/app/dashboard/work-situation/displacement/page.tsx
@@ -1,11 +1,108 @@
 "use client";
 
+import { SubmitHandler, useForm } from "react-hook-form";
+import { LuArrowLeft, LuArrowRight, LuAsterisk, LuFileUp } from "react-icons/lu";
+
 export type IForm = {
-  name: string;
+  tipo: string;
+  documentoRotacion: string;
+  fechaDesplazamiento: string;
+  oficinaOrigen: string;
+  oficinaDestino: string;
 };
 
 const Form = () => {
-  return <div>form</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit: SubmitHandler<IForm> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="font-inter">
+      <div className="flex flex-col gap-2 mb-4">
+        <label htmlFor="tipo" className="flex flex-row font-inter font-semibold capitalize">
+          tipo de desplazamiento
+          <LuAsterisk />
+        </label>
+        <input
+          type="text"
+          placeholder="ingrese el tipo de desplazamiento"
+          {...register("tipo", { required: true })}
+          className="rounded-xl font-poppins"
+        />
+        <span className="font-montserrat font-semibold text-[#d20f39] text-sm">{errors.tipo?.message}</span>
+      </div>
+
+      <div className="flex flex-row items-center gap-2 w-full">
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="documentoRotacion" className="flex flex-row font-inter font-semibold text-nowrap capitalize">
+            documento de rotación
+            <LuAsterisk />
+          </label>
+          <div className="flex flex-row items-center gap-2 p-2 border rounded-xl">
+            <LuFileUp />
+            <input
+              className="flex rounded-xl w-full cursor-pointer focus:outline-none"
+              type="file"
+              {...register("documentoRotacion", { required: true })}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="fechaDesplazamiento" className="flex flex-row font-inter font-semibold capitalize">
+            fecha del desplazamiento
+            <LuAsterisk />
+          </label>
+          <input type="date" {...register("fechaDesplazamiento", { required: true })} className="rounded-xl" />
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center gap-2 w-full">
+        <div className="flex flex-col gap-2 mb-4 w-1/2">
+          <label htmlFor="oficinaOrigen" className="flex flex-row font-inter font-semibold capitalize">
+            de la oficina
+            <LuAsterisk />
+          </label>
+          <input
+            type="text"
+            placeholder="ingrese la oficina de origen"
+            {...register("oficinaOrigen", { required: true })}
+            className="rounded-xl font-poppins"
+          />
+          <span className="font-montserrat font-semibold text-[#d20f39] text-sm">{errors.oficinaOrigen?.message}</span>
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4 w-1/2">
+          <label htmlFor="oficinaDestino" className="flex flex-row font-inter font-semibold capitalize">
+            a la oficina
+            <LuAsterisk />
+          </label>
+          <input
+            type="text"
+            placeholder="ingrese la oficina de destino"
+            {...register("oficinaDestino", { required: true })}
+            className="rounded-xl font-poppins"
+          />
+          <span className="font-montserrat font-semibold text-[#d20f39] text-sm">{errors.oficinaDestino?.message}</span>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center items-center gap-4 font-montserrat text-[#eff1f5] text-center">
+        <button type="button" className="flex flex-row items-center gap-2 bg-[#e64553] hover:bg-[#fe640b] px-4 p-2 rounded-lg text-lg uppercase">
+          <LuArrowLeft />
+          regresar
+        </button>
+        <button type="submit" className="flex flex-row items-center gap-2 bg-[#179299] hover:bg-[#40a02b] px-4 p-2 rounded-lg text-lg uppercase">
+          siguiente
+          <LuArrowRight />
+        </button>
+      </div>
+    </form>
+  );
 };
 
 const Page = () => {
