507f8901f8abf1ca276be7b06694ed8f7068304a
lc <lcommander101010@gmail.com>
2025-02-10 10:52:49 -0500

    :(

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,7 +3,8 @@ import UserInfo from "../cards/UserInfo";
 import SidebarMenuGroup from "../menus/SidebarMenuGroup";
 import { navigationItems } from "@/utils/navigation-items";
 import Link from "next/link";
-import { FaHome } from "react-icons/fa";
+import Image from "next/image";
+import logo from "@/assets/images/logo_gobierno_regional_claro.webp";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -14,9 +15,7 @@ export default function Sidebar({ isOpen, isMobile }: SidebarProps) {
   const [openMenu, setOpenMenu] = useState<string | null>(null);
   return (
     <>
-      {isOpen && isMobile && (
-        <div className="top-0 left-0 z-40 absolute bg-black/30 backdrop-blur-sm w-full h-full"></div>
-      )}
+      {isOpen && isMobile && <div className="top-0 left-0 z-40 absolute bg-black/30 backdrop-blur-sm w-full h-full"></div>}
       <aside
         className={`
         ${isOpen ? "translate-x-0" : "-translate-x-full"}
@@ -27,25 +26,22 @@ export default function Sidebar({ isOpen, isMobile }: SidebarProps) {
       >
         <div className="flex flex-col gap-6 py-4 pl-4 w-full h-full">
           <div className="relative mr-4">
-            <Link href={"/"} className="flex flex-row items-center gap-2">
-              <FaHome className="mx-2" />
-              <h1 className="font-bold text-text-primary text-xl">WorkTrace</h1>
+            <Link href={"/"} className="flex flex-row items-center gap-2 bg-red-600 px-2 rounded-md">
+              <Image
+                src={logo}
+                alt="Logo Gobierno Regional"
+                className="bg-red-600"
+                width={200} // Ancho de la imagen
+                height={100} // Alto de la imagen
+              />
             </Link>
 
-            <span className="top-0 left-36 absolute bg-green-700/15 px-2 py-1 rounded-full font-medium text-button-confirm text-xs">
-              v1.0.0
-            </span>
+            {/* <span className="top-0 left-36 absolute bg-green-700/15 px-2 py-1 rounded-full font-medium text-button-confirm text-xs">v1.0.0</span> */}
           </div>
           <UserInfo />
           <div className="pr-2 h-full overflow-y-auto">
             {navigationItems.map((group, idx) => (
-              <SidebarMenuGroup
-                key={idx}
-                title={group.title}
-                items={group.menuItem}
-                openMenu={openMenu}
-                setOpenMenu={setOpenMenu}
-              />
+              <SidebarMenuGroup key={idx} title={group.title} items={group.menuItem} openMenu={openMenu} setOpenMenu={setOpenMenu} />
             ))}
           </div>
         </div>
