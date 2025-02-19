50b81cdea4e579f65e64d34c2a51c505aec59c10
lc <lcommander101010@gmail.com>
2025-02-19 15:06:04 -0500

    other changes

--- src/app/dashboard/personal-file/personal-information/children-data/page.tsx
+++ src/app/dashboard/personal-file/personal-information/children-data/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { hijoSchema } from "@/lib/schemas/hijo.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
@@ -95,6 +97,40 @@ const FormHijo = () => {
               )}
             />
           </div>
+        </div>
+        <FormField
+          control={form.control}
+          name="gradoInstruccion"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Grado de instruccion</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Grado de instruccion" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="sin instruccion">Sin Instruccion</SelectItem>
+                  <SelectItem value="primaria completa">Primaria Completa</SelectItem>
+                  <SelectItem value="primaria incompleta">Primaria Incompleta</SelectItem>
+                  <SelectItem value="secundaria completa">Secundaria Completa</SelectItem>
+                  <SelectItem value="secundaria incompleta">Secundaria Incompleta</SelectItem>
+                  <SelectItem value="tecnivo">Tecnico</SelectItem>
+                  <SelectItem value="universitario">Universitario</SelectItem>
+                  <SelectItem value="posgrado">Posgrado</SelectItem>
+                  <SelectItem value="null">Prefiero no decirlo</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex justify-end">
+          <Button type="submit" className="justify-end bg-[#d20f39] hover:bg-[#e64553]">
+            Guardar
+          </Button>
         </div>
       </form>
     </Form>
