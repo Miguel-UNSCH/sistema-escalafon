5d470aa29b6df8a20ae781ef250d27231667cf9d
lc <lcommander101010@gmail.com>
2025-02-25 15:38:46 -0500

    capacitacion form basic

---
+++ src/app/(protected)/dashboard/personal-file/studies-training/training/FormTraining.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { capacitacionSchema } from "@/validations/personalFileSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ZCapacitacion } from "@/lib/schemas/capacitacion.schema";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { TextField } from "@/components/forms/InputTypes";
+import { DatePicker } from "@/components/forms/DateTypes";
+
+interface FormTrainingProps {
+  personalId: number;
+}
+
+export const FormTraining = ({ personalId }: FormTrainingProps) => {
+  const form = useForm<ZCapacitacion>({
+    resolver: zodResolver(capacitacionSchema),
+    defaultValues: {
+      personalId,
+      centroCapacitacion: "",
+      materia: "",
+      especialidad: "",
+      periodo: {
+        from: undefined,
+        to: undefined,
+      },
+      horasLectivas: 0,
+      fechaEmision: undefined,
+      certificadoPdf: "",
+    },
+  });
+
+  const onSubmit = async (data: ZCapacitacion) => console.log(data);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <TextField control={form.control} name="centroCapacitacion" label="Centro de capacitacion *" placeholder="Ingrese el centro donde realizo la capacitacion" />
+
+        <TextField control={form.control} name="materia" label="Materia *" placeholder="Ingrese la materia" />
+
+        <TextField control={form.control} name="especialidad" label="Especialidad *" placeholder="Ingrese la especialidad" />
+
+        <div className="gap-4 grid grid-cols-2">
+          <DatePicker control={form.control} name="periodo.from" label="Fecha de inicio" disabled={false} />
+          <DatePicker control={form.control} name="periodo.to" label="Fecha de culminacion" disabled={false} />
+        </div>
+
+        <TextField control={form.control} name="horasLectivas" label="Horas lectivas" placeholder="Ingrese las horas lectivas" />
+
+        <DatePicker control={form.control} name="fechaEmision" label="Fecha de emision del certificado" disabled={false} />
+
+        <TextField control={form.control} name="certificadoPdf" label="Certificado PDF" placeholder="Ingrese el enlace del certificado" />
+
+        <div className="flex justify-end">
+          <Button type="submit" onClick={() => console.log(form.getValues())} className="justify-end bg-[#d20f39] hover:bg-[#e64553]">
+            Guardar
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
--- src/lib/schemas/capacitacion.schema.ts
+++ src/lib/schemas/capacitacion.schema.ts
@@ -1,16 +1,22 @@
-import { object, string, number, date, z } from "zod";
+import { z } from "zod";
 
-export const capacitacionSchema = object({
-  personalId: string().min(1, "El ID del personal es obligatorio"),
-  centroCapacitacion: string().min(1, "El centro de capacitación es obligatorio"),
-  materia: string().min(1, "La materia es obligatoria"),
-  especialidad: string().min(1, "La especialidad es obligatoria"),
-  periodo: string()
-    .regex(/^\d{2}\/\d{2}\/\d{4} - \d{2}\/\d{2}\/\d{4}$/, "El formato del periodo debe ser 'dd/mm/yyyy - dd/mm/yyyy'")
-    .min(1, "El periodo es obligatorio"),
-  horasLectivas: number().min(1, "Las horas lectivas deben ser un número positivo"),
-  fechaEmision: date().min(new Date(1900, 0, 1), "La fecha de emisión debe ser válida"),
-  certificadoPdf: string().min(1, "El enlace al certificado PDF es obligatorio"),
+const periodoSchema = z.object({
+  from: z.preprocess((val) => (typeof val === "string" ? new Date(val) : val), z.date()),
+  to: z.preprocess((val) => (typeof val === "string" ? new Date(val) : val), z.date()),
 });
 
-export type Capacitacion = z.infer<typeof capacitacionSchema>;
+export const capacitacionSchema = z.object({
+  personalId: z.number().min(1, "El ID del personal es obligatorio"),
+  centroCapacitacion: z.string().min(1, "El centro de capacitación es obligatorio"),
+  materia: z.string().min(1, "La materia es obligatoria"),
+  especialidad: z.string().min(1, "La especialidad es obligatoria"),
+  periodo: periodoSchema, // Se valida como un objeto JSON con { from, to }
+  horasLectivas: z.number().min(1, "Las horas lectivas deben ser un número positivo"),
+  fechaEmision: z
+    .string({ required_error: "Fecha de emision" })
+    .refine((date) => !isNaN(Date.parse(date)), "Fecha inválida")
+    .transform((date) => new Date(date)),
+  certificadoPdf: z.string().min(1, "El enlace al certificado PDF es obligatorio"),
+});
+
+export type ZCapacitacion = z.infer<typeof capacitacionSchema>;
