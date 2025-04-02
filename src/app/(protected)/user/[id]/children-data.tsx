cfda78dc95f7f7fd3e3ed3b50712c7ba664b3b5a
lc <lcommander101010@gmail.com>
2025-04-01 21:17:29 -0500

    change file directori at user folder

--- src/app/(protected)/user/[id]/children-data.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ModuleChildren = () => {
-  return <div>children-data</div>;
-};
--- src/app/(protected)/user/[id]/personal-data.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ModulePersonal = () => {
-  return <div>personal-data</div>;
-};
