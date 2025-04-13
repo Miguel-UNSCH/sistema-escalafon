f67c41c61f95b12587f6ba20675b415419e780d7
lc <lcommander101010@gmail.com>
2025-04-13 16:55:13 -0500

    remove tex - pdf [fail]

--- src/app/(protected)/reports/time/page.tsx
+++ src/app/(protected)/reports/time/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { fn_tex_compile } from "@/actions/tex-action";
 import { Button } from "@/components/ui/button";
-import { generateLatexZip } from "@/utils/zip-reporter";
 import { ArrowLeft, CloudDownload } from "lucide-react";
 import React, { useState } from "react";
 import { FnA } from "./fn-a";
@@ -14,31 +12,11 @@ const page = () => {
 
   const handleBack = () => setuser_id("");
 
-  const handleTexToPdf = async () => {
-    const tipo = "time";
-    const nombre = "usuario123";
-
-    await generateLatexZip(nombre);
-
-    const url = await fn_tex_compile(tipo, nombre);
-
-    window.open(url, "_blank");
-  };
-
-  const handleZipGenerate = async () => {
-    await generateLatexZip("usuario123");
-  };
-
   return (
     <div className="p-5 w-full h-full">
       <div className="flex flex-col w-full h-full">
         <div className="flex flex-col items-center py-5 text-text">
           <h2 className="hover:border-text hover:border-b-4 font-primary font-bold text-2xl uppercase">computo de tiempo</h2>
-        </div>
-
-        <div className="flex flex-row items-end gap-4">
-          <Button onClick={handleTexToPdf}>generar reporte</Button>
-          <Button onClick={handleZipGenerate}>generar zip</Button>
         </div>
 
         <div className="flex flex-col gap-4 p-4 px-8">
