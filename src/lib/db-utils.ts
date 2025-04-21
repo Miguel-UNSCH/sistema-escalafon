289eeabc02e74f17f68ee6282ebe0f6d3bc24a84
lc <lcommander101010@gmail.com>
2025-04-21 08:00:44 -0500

    solver error in 'bun run buid'

--- src/lib/db-utils.ts
+++ src/lib/db-utils.ts
@@ -2,14 +2,14 @@ import { prisma } from "@/config/prisma.config";
 
 // lib/db-utils.ts
 export const isUCDInUse = async (usuarioCargoDependenciaId: number): Promise<boolean> => {
-  const [expCount] = await Promise.resolve([
+  const [ascensoCount, desplazamientoCount] = await Promise.all([
     // const [expCount, contratoCount, ascensoCount, desplazamientoCount] = await Promise.all([
-    prisma.experience.count({ where: { usuarioCargoDependenciaId } }),
+    // prisma.experience.count({ where: { usuarioCargoDependenciaId } }),
     // prisma.contrato.count({ where: { usuarioCargoDependenciaId } }),
-    // prisma.ascenso.count({ where: { usuarioCargoDependenciaId } }),
-    // prisma.desplazamiento.count({ where: { usuarioCargoDependenciaId } }),
+    prisma.ascenso.count({ where: { currentUCDId: usuarioCargoDependenciaId } }),
+    prisma.desplazamiento.count({ where: { currentUCDId: usuarioCargoDependenciaId } }),
   ]);
 
   // return expCount > 0 || contratoCount > 0 || ascensoCount > 0 || desplazamientoCount > 0;
-  return (await expCount) > 0;
+  return ascensoCount > 0 || desplazamientoCount > 0;
 };
